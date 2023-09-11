479f30cb77d3e926507b8559c97eea3f92fdd596
Its-Japo <pue22296@uvg.edu.gt>
2023-09-11 16:51:08 -0600

    selector del wallet y visor del saldo agregados

--- proyecto/app/app/src/main/java/com/example/quenotesalgacaro/MainActivity.kt
+++ proyecto/app/app/src/main/java/com/example/quenotesalgacaro/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.quenotesalgacaro
 
 import android.os.Bundle
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,18 +22,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.quenotesalgacaro.ui.theme.QueNoTeSalgaCaroTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,9 +57,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val context = LocalContext.current
-    val coffeeDrinks = arrayOf("Americano", "Cappuccino", "Espresso", "Latte", "Mocha")
+    val dates = arrayOf("Ago 2023", "Sep 2023", "Oct 2023", "Nov 2023", "Dic 2023")
+    val wallets = arrayOf("Wallet 1", "Wallet 2", "Wallet 3", "Wallet 4", "Wallet 5")
     var expanded by remember { mutableStateOf(false) }
-    var selectedText by remember { mutableStateOf(coffeeDrinks[0]) }
+    var selectedDate by remember { mutableStateOf(dates[0]) }
+    var selectedWallet by remember { mutableStateOf(wallets[0]) }
+
+    var saldo = 0.00
 
     Column (
         modifier = Modifier.fillMaxWidth()
@@ -80,35 +87,98 @@ fun MainScreen() {
             expanded = expanded,
             onExpandedChange = {
                 expanded = !expanded
-            }
+            },
+            modifier = Modifier
+                .padding(12.dp)
+                .align(alignment = Alignment.CenterHorizontally),
+
         ) {
 
             TextField(
-                value = selectedText,
+                value = selectedDate,
                 onValueChange = {},
                 readOnly = true,
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                 modifier = Modifier
                     .menuAnchor()
-                    .width(200.dp),
+                    .width(180.dp),
                 label = {
                     Text(
                         text = "Fecha"
                     )
-                }
+                },
             )
             ExposedDropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false }
             ) {
-                coffeeDrinks.forEach { item ->
+                dates.forEach { item ->
                     DropdownMenuItem(
                         text = { Text(text = item) },
                         onClick = {
-                            selectedText = item
+                            selectedDate = item
                             expanded = false
                             Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
                         }
+                    )
+                }
+            }
+        }
+        Row () {
+            Column {
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = {
+                        expanded = !expanded
+                    },
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+
+                    ) {
+
+                    TextField(
+                        value = selectedWallet,
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        modifier = Modifier
+                            .menuAnchor()
+                            .width(150.dp),
+                        label = {
+                            Text(
+                                text = "Wallet"
+                            )
+                        },
+                    )
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        wallets.forEach { item ->
+                            DropdownMenuItem(
+                                text = { Text(text = item) },
+                                onClick = {
+                                    selectedWallet = item
+                                    expanded = false
+                                    Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                                }
+                            )
+                        }
+                    }
+                }
+                Column {
+                    Text (
+                        text = "Saldo",
+                        modifier = Modifier.padding(12.dp),
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Text (
+                        text = "Q$saldo",
+                        modifier = Modifier.padding(12.dp),
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onSurface
                     )
                 }
             }
