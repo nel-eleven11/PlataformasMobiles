5240cc989ff285110581c21201c8c133a8c03385
Its-Japo <pue22296@uvg.edu.gt>
2023-09-11 18:48:06 -0600

    Dashboard de la vista terminado

--- proyecto/app/app/src/main/java/com/example/quenotesalgacaro/MainActivity.kt
+++ proyecto/app/app/src/main/java/com/example/quenotesalgacaro/MainActivity.kt
@@ -5,8 +5,10 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,6 +19,8 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -30,7 +34,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,34 +65,61 @@ fun MainScreen() {
     val context = LocalContext.current
     val dates = arrayOf("Ago 2023", "Sep 2023", "Oct 2023", "Nov 2023", "Dic 2023")
     val wallets = arrayOf("Wallet 1", "Wallet 2", "Wallet 3", "Wallet 4", "Wallet 5")
-    var expanded by remember { mutableStateOf(false) }
+    var expandedDate by remember { mutableStateOf(false) }
+    var expandedWallet by remember { mutableStateOf(false) }
     var selectedDate by remember { mutableStateOf(dates[0]) }
     var selectedWallet by remember { mutableStateOf(wallets[0]) }
 
-    var saldo = 0.00
+    val saldo = 7430.41
+    val total = 12000.00
+    val progress = (total - saldo)/total
 
     Column (
         modifier = Modifier.fillMaxWidth()
     ) {
-        Surface (
-            modifier = Modifier.fillMaxWidth(),
-            color = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary
-        ) {
-            Text(
-                text = "Que no te salga caro",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                style = MaterialTheme.typography.titleLarge,
-                textAlign = TextAlign.Center,
+        Box {
+            Surface (
+                modifier = Modifier.fillMaxWidth(),
+                color = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ) {
+                Text(
+                    text = "Que no te salga caro",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
 
-            )
+                )
+            }
+            Box (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.CenterEnd
+            ){
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterStart)
+                        .width(48.dp)
+                    ) {
+                    Icon(
+                        painter = painterResource(
+                            id = R.drawable.outline_settings_24
+                        ),
+                        contentDescription = "Settings",
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+
+                }
+            }
         }
+
         ExposedDropdownMenuBox(
-            expanded = expanded,
+            expanded = expandedDate,
             onExpandedChange = {
-                expanded = !expanded
+                expandedDate = !expandedDate
             },
             modifier = Modifier
                 .padding(12.dp)
@@ -98,7 +131,7 @@ fun MainScreen() {
                 value = selectedDate,
                 onValueChange = {},
                 readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedDate) },
                 modifier = Modifier
                     .menuAnchor()
                     .width(180.dp),
@@ -109,27 +142,31 @@ fun MainScreen() {
                 },
             )
             ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
+                expanded = expandedDate,
+                onDismissRequest = { expandedDate = false }
             ) {
                 dates.forEach { item ->
                     DropdownMenuItem(
                         text = { Text(text = item) },
                         onClick = {
                             selectedDate = item
-                            expanded = false
+                            expandedDate = false
                             Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
                         }
                     )
                 }
             }
         }
-        Row () {
-            Column {
+        Row {
+            Column (
+                modifier = Modifier
+                    .weight(5f)
+                    .padding(12.dp),
+            ){
                 ExposedDropdownMenuBox(
-                    expanded = expanded,
+                    expanded = expandedWallet,
                     onExpandedChange = {
-                        expanded = !expanded
+                        expandedWallet = !expandedWallet
                     },
                     modifier = Modifier
                         .padding(12.dp)
@@ -141,10 +178,9 @@ fun MainScreen() {
                         value = selectedWallet,
                         onValueChange = {},
                         readOnly = true,
-                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedWallet) },
                         modifier = Modifier
-                            .menuAnchor()
-                            .width(150.dp),
+                            .menuAnchor(),
                         label = {
                             Text(
                                 text = "Wallet"
@@ -152,35 +188,62 @@ fun MainScreen() {
                         },
                     )
                     ExposedDropdownMenu(
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false }
+                        expanded = expandedWallet,
+                        onDismissRequest = { expandedWallet = false }
                     ) {
                         wallets.forEach { item ->
                             DropdownMenuItem(
                                 text = { Text(text = item) },
                                 onClick = {
                                     selectedWallet = item
-                                    expanded = false
+                                    expandedWallet = false
                                     Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
                                 }
                             )
                         }
                     }
                 }
-                Column {
+                Column (
+                    modifier = Modifier.padding(12.dp),
+                ){
                     Text (
                         text = "Saldo",
-                        modifier = Modifier.padding(12.dp),
-                        style = MaterialTheme.typography.labelSmall,
-                        color = MaterialTheme.colorScheme.onSurface
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                     Text (
                         text = "Q$saldo",
-                        modifier = Modifier.padding(12.dp),
-                        style = MaterialTheme.typography.titleLarge,
+                        style = MaterialTheme.typography.displaySmall,
                         color = MaterialTheme.colorScheme.onSurface
                     )
                 }
+            }
+            Box (
+                modifier = Modifier.weight(4f),
+            ) {
+                CircularProgressIndicator(
+                    progress = progress.toFloat(),
+                    modifier = Modifier
+                        .width(160.dp)
+                        .aspectRatio(1f)
+                        .align(alignment = Alignment.Center)
+                        .padding(12.dp),
+                    strokeWidth = 16.dp,
+                    color = when(progress) {
+                        in 0.0..0.5 -> Color(57,255,20)
+                        in 0.5..0.8 -> Color(255,255,0)
+                        in 0.8..0.99 -> Color(255,0,0)
+                        else -> {
+                            Color(80,0,0)
+                        }
+                    }
+                )
+                Text (
+                    text = "${(progress*100).toInt()}%",
+                    style = MaterialTheme.typography.displaySmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.align(alignment = Alignment.Center)
+                )
             }
         }
     }
